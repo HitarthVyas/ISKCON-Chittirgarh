c8dd91ab10fb3f7ec4d0bdef98dbd38290f9163a
vipinsharma0999 <136472185+vipinsharma0999@users.noreply.github.com>
2024-06-21 17:18:07 +0530

    f

---
+++ src/app/(donation)/vigrah-seva/page.js
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import MainPoster from '@/components/Donation/VigrahSeva/01MainPoster/MainPoster'
+
+
+const page = () => {
+  return (
+    <div>
+        <MainPoster />
+    </div>
+  )
+}
+
+export default page
